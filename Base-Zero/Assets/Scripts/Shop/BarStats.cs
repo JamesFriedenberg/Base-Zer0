3fab53384aaf2fc2d2abd192ea52d014dddfd634
James <jpf9146>
2019-03-23 18:24:41 -0400

    Shop design mockup finished

---
+++ Base-Zero/Assets/Scripts/Shop/BarStats.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BarStats : MonoBehaviour {
+	public Image statBar;
+	public float stat;
+	// Use this for initialization
+	void Start () {
+		statBar.fillAmount = (1.0f * (stat / 100.0f));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
