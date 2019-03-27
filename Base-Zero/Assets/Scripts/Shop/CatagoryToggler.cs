7cb2f8f3ec86e80bd2b3e45b28b32a11c12cf842
James <jpf9146>
2019-03-27 15:16:40 -0400

    Worked on shop, fixed some shop bug

---
+++ Base-Zero/Assets/Scripts/Shop/CatagoryToggler.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatagoryToggler : MonoBehaviour {
+	GameObject[] weapons;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void ToggleActiveWeapons(int index){
+		for (int i = 0; i < weapons.Length; i+= 2) {
+			if (i != index) {
+				weapons [i].SetActive (false);
+				weapons [i + 1].SetActive (false);
+			}
+		}
+
+		weapons [index].SetActive (true);
+		weapons [index + 1].SetActive (true);
+	}
+}
