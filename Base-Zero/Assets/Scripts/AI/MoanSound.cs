3508b9545e65f7745cbb3705bea2253ecf0cdba4
James Friedenberg <jpf9146@g.rit.edu>
2019-04-02 19:45:34 -0400

    added audio, added zombie moaning (broken), fixed vehicle depot terrain

---
+++ Base-Zero/Assets/Scripts/AI/MoanSound.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoanSound : MonoBehaviour {
+
+    public AudioClip moanSound;
+
+    private AudioSource source;
+    private float volLowRange = .75f;
+    private float volHighRange = 1.0f;
+    private int moanChance;
+
+
+
+	// Use this for initialization
+	void Awake () {
+        source = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Vector3.Distance(this.transform.position, GameObject.FindGameObjectWithTag("Player").transform.position) < 10f){
+            moanChance = Random.Range(0, 10);
+            float vol = Random.Range(volLowRange, volHighRange);
+            if(moanChance > 5)
+            {
+                source.PlayOneShot(moanSound, vol);
+            }
+        }
+	}
+}
