600f60ee1fa1985a3de4e398652b1a0d4554e05b
James Friedenberg <jpf9146@g.rit.edu>
2019-04-21 07:29:53 -0400

    finished trainyard and started a medkit script

---
+++ Base-Zero/Assets/Scripts/HealthKit.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthKit : MonoBehaviour {
+
+    public GameObject player;
+    private GameObject gameManager;
+    private GameManager gm;
+    private int currentPlayerHealth;
+
+
+    // Use this for initialization
+    void Start () {
+        player = GameObject.FindWithTag("Player");
+        //take stats from gm at start of scene
+        gameManager = GameObject.FindGameObjectWithTag("gm");
+        gm = gameManager.GetComponent<GameManager>();
+        currentPlayerHealth = gm.currentPlayerHealth;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(currentPlayerHealth <= 90)
+        {
+            currentPlayerHealth += 10;
+        }
+        else
+        {
+            currentPlayerHealth += 100 - currentPlayerHealth;
+        }
+
+        Destroy(this.gameObject);
+    }
+}
