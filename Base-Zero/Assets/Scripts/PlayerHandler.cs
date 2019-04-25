03296612a2ae7e119098e38920e391f9ea4a2879
Noah Wilson <ntw9049@g.rit.edu>
2019-04-25 17:37:18 -0400

    fixed bug that prevented player from firing if they switched weapons while running and trying to fire

--- Base-Zero/Assets/Scripts/PlayerHandler.cs
+++ Base-Zero/Assets/Scripts/PlayerHandler.cs
@@ -71,7 +71,7 @@ public class PlayerHandler : MonoBehaviour
     void Update()
     {
         
-        if (Input.GetButton("Fire2")) return;
+        if (Input.GetButton("Fire2") || playerWeapons[currentWeapon].GetComponentInChildren<weapon>().WillFire()) return;
         if (Input.GetKeyDown("1"))
         {
             SwitchWeapon(0);
