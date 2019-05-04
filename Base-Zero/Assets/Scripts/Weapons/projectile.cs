9430fed735807006b3612b3cd7dbac637bb27336
Noah Wilson <ntw9049@g.rit.edu>
2019-05-04 19:06:26 -0400

    added damage to player when they fire projectile near them

--- Base-Zero/Assets/Scripts/Weapons/projectile.cs
+++ Base-Zero/Assets/Scripts/Weapons/projectile.cs
@@ -7,7 +7,7 @@ public class projectile : MonoBehaviour
 
     public float speed = 10;
     public int damage = 100;
-    public float explosionRange = 2.0f;
+    public float explosionRange = 3.0f;
     public GameObject particleTrail;
     public GameObject impactExplosion;
     public Transform emitter;
@@ -49,6 +49,16 @@ public class projectile : MonoBehaviour
                 {
                     enemies[i].GetComponent<ZombieController>().takeDamage(myDamage);
                 }
+            }
+        }
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        float pDistance = Vector3.Magnitude(player.transform.position - this.transform.position);
+        if (pDistance <= explosionRange * 1.5f)
+        {
+            float myDamage = ((Mathf.Pow(explosionRange * 1.5f, 2) - Mathf.Pow(pDistance, 2)) / Mathf.Pow(explosionRange * 1.5f, 2)) * damage * 4;
+            if (player.GetComponent<PlayerHandler>())
+            {
+                player.GetComponent<PlayerHandler>().TakeDamage((int)myDamage);
             }
         }
         DoDestroy();
